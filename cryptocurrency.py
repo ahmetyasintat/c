f46412e314d5d955f92708c6f9e3ecf0b041008b
Ahmet Yasin <93879643+ahmetyasintat@users.noreply.github.com>
2023-03-17 15:56:53 +0300

    Update

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -14,26 +14,32 @@ async def main():
     async with aiohttp.ClientSession() as session:
         start_time = time.time()
         count = 0
-        rows = 30
-        columnwidth = 20
+        rows = 20
+        columnwidth = 30
         randchars = ['*','%','$','&','@','!','^','~','+','?','/','|','<','>']
         os.system('clear')
         
+        sys.stdout.write("\033[6;0H")
+        sys.stdout.write("\033[34mTicker    Price      CHG%\n")
+        sys.stdout.flush()
+        
         for i in range(len(tickers)):
             k = i // rows
-            sys.stdout.write("\033[%d;%dH" % (i - rows*k, k*columnwidth))
+            sys.stdout.write("\033[%d;%dH" % (i+7 - rows*k, k*columnwidth)) 
             sys.stdout.write("\033[33m%s%s" % (tickers[i], "-" * (columnwidth - len(tickers[i]))))
-
+        
         while True:
             tasks = []
             for ticker in tickers:
-                task = asyncio.ensure_future(fetch(session, 'https://api.binance.com/api/v3/ticker/price?symbol={}'.format(ticker)))
+                task = asyncio.ensure_future(fetch(session, 'https://api.binance.com/api/v3/ticker/24hr?symbol={}'.format(ticker)))
                 tasks.append(task)
             responses = await asyncio.gather(*tasks)
             for response in responses:
-                xposition = 7 + (tickers.index(response['symbol']) // rows) * columnwidth
-                sys.stdout.write("\033[%d;%dH" % (tickers.index(response['symbol']) % rows,xposition))
-                sys.stdout.write("\033[37m%.2f%s" % (float(response['price']) ,randchars[random.randint(0, len(randchars) - 1)]))
+                xposition = 11 + (tickers.index(response['symbol']) // rows) * columnwidth
+                sys.stdout.write("\033[%d;%dH" % (tickers.index(response['symbol']) % rows+7,xposition))
+                sys.stdout.write("\033[37m%.2f%s" % (float(response['lastPrice']) ,randchars[random.randint(0, len(randchars) - 1)]))
+                sys.stdout.write("\033[%d;%dH" % (tickers.index(response['symbol']) % rows+7, 22))
+                sys.stdout.write("\033[37m%.2f%s" % (float(response['priceChangePercent']) ,"%"))
                 if count % 250 == 0 and count > 0:
                     sys.stdout.write("\033[45;3H")
                     sys.stdout.write("Data Received: {}".format(count))
@@ -45,8 +51,9 @@ async def main():
                     sys.stdout.write("Rate: {}x".format(int(count / (end_time - start_time))))
                 sys.stdout.flush()
                 count += 1
-            time.sleep(0.5)
-            
+            time.sleep(1)
+
+
 loop = asyncio.get_event_loop()
 future = asyncio.ensure_future(main())
 loop.run_until_complete(future)
